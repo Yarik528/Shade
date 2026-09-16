305d0fae19e8515a1b6d34f82b34dd5be2591c40
Yarik528 <fonarik@duck.com>
2026-09-16 17:45:50 +0700

    Create BlockRenderManagerMixin.java

---
+++ src/main/java/com/shade/mixin/BlockRenderManagerMixin.java
@@ -0,0 +1,20 @@
+// language: Java, file: src/main/java/com/shade/mixin/BlockRenderManagerMixin.java
+package com.shade.mixin;
+
+import com.shade.module.XRay;
+import net.minecraft.block.BlockState;
+import net.minecraft.client.render.block.BlockRenderManager;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(BlockRenderManager.class)
+public class BlockRenderManagerMixin {
+    @Inject(method = "shouldDrawSide", at = @At("HEAD"), cancellable = true)
+    private static void onShouldDrawSide(BlockState state, CallbackInfoReturnable<Boolean> cir) {
+        if (!XRay.shouldRender(state.getBlock())) {
+            cir.setReturnValue(false);
+        }
+    }
+}
