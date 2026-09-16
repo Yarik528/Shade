1d041440c1af6fec22c63c03e8ccdeb117857c2b
Yarik528 <fonarik@duck.com>
2026-09-16 17:52:18 +0700

    Create MinecraftClientMixin.java

---
+++ src/main/java/com/shade/mixin/MinecraftClientMixin.java
@@ -0,0 +1,18 @@
+// language: Java, file: src/main/java/com/shade/mixin/MinecraftClientMixin.java
+package com.shade.mixin;
+
+import com.shade.config.ConfigManager;
+import net.minecraft.client.MinecraftClient;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(MinecraftClient.class)
+public class MinecraftClientMixin {
+
+    @Inject(method = "stop", at = @At("HEAD"))
+    private void onStop(CallbackInfo ci) {
+        ConfigManager.save();
+    }
+}
