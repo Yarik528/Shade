835d2b0ac4c7101e0fb40a540cacb5e5ddd3bb57
Yarik528 <fonarik@duck.com>
2026-09-17 12:19:20 +0700

    Create ClientPlayerEntityMixin.java

---
+++ src/main/java/com/shade/mixin/ClientPlayerEntityMixin.java
@@ -0,0 +1,28 @@
+package com.shade.mixin;
+
+import com.shade.module.ModuleManager;
+import com.shade.module.SilentAim;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(ClientPlayerEntity.class)
+public class ClientPlayerEntityMixin {
+
+    @Redirect(method = "sendMovementPackets",
+              at = @At(value = "NEW",
+                       target = "(DDDFFZ)Lnet/minecraft/network/packet/c2s/play/PlayerMoveC2SPacket$LookAndOnGround;"))
+    private PlayerMoveC2SPacket.LookAndOnGround silentLook(
+            double x, double y, double z, float yaw, float pitch, boolean onGround) {
+
+        SilentAim aim = (SilentAim) ModuleManager.INSTANCE.byName("SilentAim");
+        if (aim != null && aim.isEnabled()) {
+            aim.updateSilentAngles();
+            return new PlayerMoveC2SPacket.LookAndOnGround(
+                aim.getSilentYaw(), aim.getSilentPitch(), onGround);
+        }
+        return new PlayerMoveC2SPacket.LookAndOnGround(yaw, pitch, onGround);
+    }
+}
