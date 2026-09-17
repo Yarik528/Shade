0e522289ec02d57c3604e628f6af72de30614a3b
Yarik528 <fonarik@duck.com>
2026-09-17 12:25:45 +0700

    Create InGameHudMixin.java

---
+++ src/main/java/com/shade/mixin/InGameHudMixin.java
@@ -0,0 +1,18 @@
+package com.shade.mixin;
+
+import com.shade.gui.HudRenderer;
+import net.minecraft.client.gui.hud.InGameHud;
+import net.minecraft.client.util.math.MatrixStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(InGameHud.class)
+public class InGameHudMixin {
+
+    @Inject(method = "render", at = @At("TAIL"))
+    private void onRender(MatrixStack matrices, float tickDelta, CallbackInfo ci) {
+        HudRenderer.render(matrices);
+    }
+}
