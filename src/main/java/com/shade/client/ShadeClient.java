4d2d3a739a244c6273b3fd9c960f4d61a4404185
Yarik528 <fonarik@duck.com>
2026-09-17 19:52:07 +0700

    Update ShadeClient.java

--- src/main/java/com/shade/client/ShadeClient.java
+++ src/main/java/com/shade/client/ShadeClient.java
@@ -1,10 +1,30 @@
 package com.shade.client;
 
+import com.shade.config.ConfigManager;
+import com.shade.gui.ClickGuiOpener;
+import com.shade.module.*;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 
 public class ShadeClient implements ClientModInitializer {
+
     @Override
     public void onInitializeClient() {
-        System.out.println("[Shade] loaded");
+        ModuleManager mm = ModuleManager.INSTANCE;
+        mm.register(new ESP());
+        mm.register(new XRay());
+        mm.register(new SilentAim());
+        mm.register(new Reach());
+        mm.register(new KillAura());
+        mm.register(new Velocity());
+        mm.register(new Sprint());
+
+        ConfigManager.load();
+        ClickGuiOpener.register();
+
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            if (client.player == null) return;
+            mm.tickAll();
+        });
     }
 }
