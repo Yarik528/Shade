de54f3630588797cf86042f335abb2296201dbf6
Yarik528 <fonarik@duck.com>
2026-09-18 10:50:38 +0700

    Create AltEntry.java

---
+++ src/main/java/com/shade/alt/AltEntry.java
@@ -0,0 +1,12 @@
+package com.shade.alt;
+
+public class AltEntry {
+    public String username;
+
+    public AltEntry(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() { return username; }
+    public void setUsername(String v) { username = v; }
+}
